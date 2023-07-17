342b2762715e4bddef8c71b740bfc36ad906b53a
Kitwana Akil <kakil@me.com>
2023-07-17 07:14:24 -0400

    Completed basic order details implementation.

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Kitwana Akil on 7/17/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false 
+}
+
