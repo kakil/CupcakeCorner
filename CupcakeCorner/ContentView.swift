ecae3d82a8a2418251c7ad5d7ad21c4669bd5020
Kitwana Akil <kakil@me.com>
2023-07-17 06:28:38 -0400

    Completed loading an image from a remote server

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
         }
-        .padding()
+        .frame(width: 200, height: 200)
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+           
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
     }
 }
 
