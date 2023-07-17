2ac5ef9e609a055a9f2c59e71c1b5ec468a8ca22
Kitwana Akil <kakil@me.com>
2023-07-17 06:01:07 -0400

    completed sending and receiving codable data with URLSession and SwiftUI

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+
+}
+
 struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(results, id:\.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        // Step 1 - create the url we want to read
+        guard let url = URL(string: "https://itunes.apple.com/search?term=slick+rick&entity=song") else {
+            print("Invalid url")
+            return
+        }
+        
+        // Step 2 - fetch data from the url
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
+        // Step 3 - Decode the result of that data into our response
     }
 }
 
